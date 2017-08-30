072f9b9d2a2d48f52a82bd2189fda76339aa5c09
Matthew Peixoto <matthewpeixoto@gmail.com>
2017-08-30 15:43:35 -0400

    Clean up JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-var i = 0;
-$banner = $('.banner');
+var $banner = $('.banner');
+var $star;
+var i;
 
 for (i = 0; i < 75; i++) {
-  var $star = $('<div>').addClass('star');
-
+  $star = $('<div>').addClass('star');
   $star.css({
-    'top': Math.random() * ($banner.outerHeight()),
-    'left': Math.random() * ($banner.outerWidth()),
-    'opacity': Math.random(),
-    'transform': 'rotate(' + Math.floor(Math.random() * 360) + 'deg) scale(' + Math.floor(Math.random() * 4) + ')'
+    top: Math.random() * $banner.outerHeight(),
+    left: Math.random() * $banner.outerWidth(),
+    opacity: Math.random(),
+    transform: 'rotate(' + Math.random() * 360 + 'deg) scale(' + Math.random() + ')'
   });
 
   $banner.append($star);
